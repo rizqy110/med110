58ae708131f26b93cd23057db0352e150f55b174
rizqy110 <aldinmodo@gmail.com>
2022-09-10 13:15:06 +0700

    update

--- index.php
+++ index.php
@@ -4,7 +4,32 @@ if(isset($_GET['rr-av'])) {
   $url = $_GET['rr-av'];
   header('location:source.php?url=https://kuramanime.com/anime/'.$url.'');
 } else {
- 
+
+}
+if(isset($_GET['goo'])) {
+  $url = $_GET['goo'];
+  header('location:https://drive.google.com/uc?export=download&confirm=t&id='.$url.'');
+} else {
+
+}
+if(isset($_GET['med'])) {
+  $url = $_GET['med'];
+  header('location:https://rr-anime.000webhostapp.com/med.php?url=https://www.mediafire.com/file/'.$url.'');
+} else {
+
+}
+if(isset($_GET['arc'])) {
+  $url = $_GET['arc'];
+  header('location:https://archive.org/download/'.$url.'');
+} else {
+
+}
+if(isset($_GET['kur'])) {
+  $url = $_GET['kur'];
+  $arr = array("https://miyuki.my.id/0:/", "https://megumin.my.id/0:/","https://shinoaki.my.id/0:/","https://mushoku.my.id/0:/","https://liscia.my.id/0:/","https://iino.my.id/0:/","https://kaguya.my.id/0:/","https://nagisa.my.id/0:/","https://tonikawa.my.id/0:/","https://tantei.my.id/0:/","https://maou.my.id/0:/","https://asuna.my.id/0:/","https://juliet.my.id/0:/");
+  header('location:'.$arr[array_rand($arr)].$url.'');
+} else {
+
 }
 ?>
 <!DOCTYPE html>
--- med.php
+++ med.php
@@ -1,5 +1,5 @@
 <?php
-include 'assets/simplehtmldom.php';
+/*include 'assets/simplehtmldom.php';
 if(isset($_GET['id'])){
     $url = 'https://www.mediafire.com/file/'.$_GET['id'].'/';
     $status = 1;
@@ -7,5 +7,23 @@ if(isset($_GET['id'])){
 $status = 1;
 $html = file_get_html($url);
 $directlink = $html->find('#downloadButton',0)->href;
-header('location:'.$directlink.'');
+header('location:'.$directlink.'');*/
+
+$url = isset($_GET['url']) ? htmlspecialchars($_GET['url']) : null;
+$support_domain = 'www.mediafire.com';
+if(empty($url)) {
+  $url = 'http://www.mediafire.com/file/8x5ol3r8wpb477a/small.mp4'; // sample link
+}
+if($url) {
+  preg_match('@^(?:http.?://)?([^/]+)@i', $url, $matches);
+  $host = $matches[1];
+  if($host != $support_domain) {
+    echo 'Please input a valid mediafire url.';
+    exit;
+  }
+}
+$result = file_get_contents($url, false, stream_context_create(['socket' => ['bindto' => '0:0']])); // force IPv4
+preg_match('/aria-label="Download file"\n.+href="(.*)"/', $result, $matches);
+$result = urldecode($matches[1]);
+header('location:'.$result.'');
 ?>

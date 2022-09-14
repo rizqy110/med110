c783c37f16052d7a5543299ff5dd6bc4c9f0c920
rizqy110 <aldinmodo@gmail.com>
2022-09-14 19:29:28 +0700

    update

---
+++ source2.php
@@ -0,0 +1,14 @@
+<?php
+include 'assets/simplehtmldom.php';
+header('Access-Control-Allow-Origin: *');
+if(isset($_GET['get_html'])){
+       header('Content-Type: application/json; charset=utf-8');
+       echo file_get_html($_GET['get_html']);
+       die();
+}
+
+if(isset($_GET['url'])){$url = $_GET['url'];}else{$url = "";}
+$html = file_get_html($url);
+$directlink = $html->find('source',2)->src;
+header('location:'.$directlink.'');
+?>

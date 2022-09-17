6902d198508241d1b1a112136df8ab61d382273e
rizqy110 <aldinmodo@gmail.com>
2022-09-17 14:06:33 +0700

    update

--- index.php
+++ index.php
@@ -12,6 +12,18 @@ if(isset($_GET['rr-av2'])) {
 } else {
 
 }
+if(isset($_GET['dl1'])) {
+  $url = $_GET['dl1'];
+  header('location:dl1.php?url=https://kuramanime.com/anime/'.$url.'');
+} else {
+
+}
+if(isset($_GET['dl2'])) {
+  $url = $_GET['dl2'];
+  header('location:dl2.php?url=https://kuramanime.com/anime/'.$url.'');
+} else {
+
+}
 if(isset($_GET['ngentot'])) {
   $url = $_GET['ngentot'];
   $arr = array("https://www.funhealtykey.com/2022/09/4-recipes-to-gain-weight.html","https://www.funhealtykey.com/2022/07/dangers-of-oxidized-food.html","https://www.funhealtykey.com/2022/07/your-body-is-acidic-stop-consuming-sugar.html","https://www.funhealtykey.com/2022/06/3-ways-to-balance-bodys-acidic-ph.html","https://www.funhealtykey.com/2022/06/processed-products-and-leaky-gut.html","https://www.funhealtykey.com/2022/06/how-to-stop-feeling-sleepy-after-eating.html");

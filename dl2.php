e6d688ffe776860149af68f61ec8ea1c9d98135d
rizqy110 <aldinmodo@gmail.com>
2022-09-17 13:58:30 +0700

    update

---
+++ dl2.php
@@ -0,0 +1,16 @@
+<?php
+include 'dom.php';
+header('Access-Control-Allow-Origin: *');
+if(isset($_GET['get_html'])){
+       header('Content-Type: application/json; charset=utf-8');
+       echo file_get_html($_GET['get_html']);
+       die();
+}
+
+if(isset($_GET['url'])){$url = $_GET['url'];}else{$url = "";}
+$html = file_get_html($url);
+$directlink = $html->find('.bg-white .mt-2 .mb-2 a',9)->href;
+//$dlh = file_get_html($directlink);
+//$link = $dlh->find('a',0)->href;
+header('location:'.$directlink.'');
+?>

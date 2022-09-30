d09ba3a3074e584db272786fd4544b7e4570c166
rizqy110 <aldinmodo@gmail.com>
2022-09-30 08:27:34 +0700

    update

--- dl6.php
+++ dl6.php
@@ -9,7 +9,7 @@ if(isset($_GET['get_html'])){
 
 if(isset($_GET['url'])){$url = $_GET['url'];}else{$url = "";}
 $html = file_get_html($url);
-$directlink = $html->find('.bg-white .mt-2 .mb-2 a',8)->href;
+$directlink = $html->find('.bg-white .mt-2 .mb-2 a',14)->href;
 //$dlh = file_get_html($directlink);
 //$link = $dlh->find('a',0)->href;
 header('location:'.$directlink.'');

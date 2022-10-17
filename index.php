7573aea133c8eecca85d3ac8d3d3ed337934cf06
rizqy110 <aldinmodo@gmail.com>
2022-10-17 09:27:56 +0700

    update

--- index.php
+++ index.php
@@ -15,61 +15,6 @@ header('location:'.$arr[array_rand($arr)].$url.'');
 
 }
 
-if(isset($_GET['rr-av2'])) {
-  $url = $_GET['rr-av2'];
-  header('location:source2.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl1'])) {
-  $url = $_GET['dl1'];
-  header('location:dl1.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl2'])) {
-  $url = $_GET['dl2'];
-  header('location:dl2.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl3'])) {
-  $url = $_GET['dl3'];
-  header('location:dl3.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl4'])) {
-  $url = $_GET['dl4'];
-  header('location:dl4.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl5'])) {
-  $url = $_GET['dl5'];
-  header('location:dl5.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['dl6'])) {
-  $url = $_GET['dl6'];
-  header('location:dl6.php?url=https://kuramanime.com/anime/'.$url.'');
-} else {
-
-}
-if(isset($_GET['blog'])) {
-  $url = $_GET['blog'];
-  $arr = array("https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/09/11-best-self-healing-ways-effectively-healing-inner-wounds.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/09/10-ways-to-fatten-gain-child-weight.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/09/11-Benefits-of-Propolis-Scientifically-Proven-Magical-Substance.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/09/4-recipes-to-gain-weight.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/06/how-to-stop-feeling-sleepy-after-eating.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/06/processed-products-and-leaky-gut.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/06/3-ways-to-balance-bodys-acidic-ph.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/07/your-body-is-acidic-stop-consuming-sugar.html","https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.funhealtykey.com/2022/07/dangers-of-oxidized-food.html");
-  header('location:'.$arr[array_rand($arr)].$url.'');
-} else {
- 
-}
-if(isset($_GET['rr-op'])) {
-  $url = $_GET['rr-op'];
-  header('location:iframe.php?url=https://65.108.132.145/'.$url.'');
-} else {
-
-}
 if(isset($_GET['goo'])) {
   $url = $_GET['goo'];
   header('location:https://drive.google.com/uc?export=download&confirm=t&id='.$url.'');
@@ -95,6 +40,11 @@ if(isset($_GET['kur'])) {
 } else {
 
 }
+if (isset($_GET['id'])) {
+  $id = $_GET['id']; 
+} else {
+ 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -105,10 +55,15 @@ if(isset($_GET['kur'])) {
 <link rel="stylesheet" href="style.css">
 <link rel="shortcut icon" href="https://img.icons8.com/color/1080/tanjiro-kamado.png" type="image/x-icon">
 
-<meta name="description" content="Grabber RR-Anime KU">
+<meta name="description" content="Grabber Anime Twitter">
+<style>
+  .bc-1{background-color:#1f1f1f}
+  .p-5{padding:5px}
+  .br-3{border-radius:3px}
+</style>
 
 </head>
 <body>
-  <a href="//dnime.pages.dev">Dnime Pages</a>
+  <center><a class="bc-1 p-5 br-3" href="index.php?api=<?php echo $id; ?>">Play Anime Here</a></center>
 </body>
 </html>
